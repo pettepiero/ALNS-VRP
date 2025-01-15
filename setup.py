4fddcf289d1c3380af02a6eb3a9c1dcb2caa000f
pettepiero <pette.piero@gmail.com>
2025-01-15 10:38:29 +0100

    Written setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="alns-vrp",
+    version="0.1",
+    packages=find_packages(),
+    description="Adaptive Large Neighborhood Search for VRP",
+    author="",
+    license="MIT",
+)
